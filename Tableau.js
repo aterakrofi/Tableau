0641ef60678e7e19a44e2abaa5984c79703ff271
aterakrofi <aterakrofi@gmail.com>
2018-06-05 08:47:11 -0400

    Tableau Javascript Api samples

---
+++ Tableau.js
@@ -0,0 +1,6 @@
+﻿function initViz() {
+    var containerDiv = document.getElementById("tableauViz"),
+        url = "https://public.tableau.com/views/MM21TheGuardiansEPLPredictions201718_0/MM21EPLPredictions?:embed=y&:display_count=yes";
+
+    var viz = new tableau.Viz(containerDiv, url);
+}
